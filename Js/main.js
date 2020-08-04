717958d41cc3d4178939bd0ea01474eda14d803d
vovantais <vladimir0297@mail.ru>
2020-08-04 16:22:56 +0300

    add  event scroll

--- Js/main.js
+++ Js/main.js
@@ -91,3 +91,37 @@ function Popup() {
    })
 }
 Popup();
+
+const animItems = document.querySelectorAll('.animations');
+if (animItems.length > 0) {
+   window.addEventListener('scroll', animScroll);
+   function animScroll() {
+      for (let i = 0; i < animItems.length; i++) {
+         const animItem = animItems[i];
+         const animItemHeight = animItem.offsetHeight;
+         const animItemOffset = offset(animItem).top;
+         const animStart = 4;
+         let animItemPoint = window.innerHeight - animItemHeight / animStart;
+         if (animItemHeight > window.innerHeight) {
+            animItemPoint = window.innerHeight - window.innerHeight / animStart;
+         }
+         if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+            animItem.classList.add("_active");
+         }
+         else {
+            if (!animItem.classList.contains('scrollUpHide')) {
+               animItem.classList.remove("_active");
+            }
+         }
+      }
+   }
+   function offset(el) {
+      const rect = el.getBoundingClientRect();
+      const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+         scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+   }
+   setTimeout(() => {
+      animScroll();
+   }, 300);
+}
